dfec66c89aa450678393ee985a069372f0f0dd5d
ChrisHorler <csh.sammt@gmail.com>
2025-04-11 19:09:50 +0200

    Slight API
    - Changed program.cs to try and get Azure API connection

--- backend/BackendAPI.WebApi/obj/Debug/net8.0/BackendAPI.WebApi.AssemblyInfo.cs
+++ backend/BackendAPI.WebApi/obj/Debug/net8.0/BackendAPI.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendAPI.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e549fee998554dd9f71d7d0ed843785df9c4370")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18160eab28323e9dd1bf10f6c159cce218522d5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendAPI.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendAPI.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
